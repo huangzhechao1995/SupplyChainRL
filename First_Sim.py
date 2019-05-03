bc1e377b26d5748654db8145fc27a6a868a2f3e2
jonah adler <jaadler29@gmail.com>
2019-05-02 20:02:30 -0400

    t and r funcs

---
+++ First_Sim.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def transition_stock(stock, demand, assignment):
+    new_stock = np.zeros(len(stock))
+    stock[0] = min(stock[0]+assignment[0] -
+                   np.sum(assignment[1:]), self.CWarehouse[0])
+    stock[1:] = np.minimum(stock[1:] + assignment[1:] - demand[1:], self.capacity=0), self.CWarehouse[1:])
+    return stock
+
+def reward(stock, demand, assignment):
+    r = (self.Price * np.sum(demand[1:]) - 
+    Kpr*assignment[0] - 
+    np.sum(np.maximum(stock,0)) + 
+    Kpe*np.sum(np.minimum(stock[1:],0)) - 
+    np.sum(Ktr[1:]* np.ceil(assignment[1:]/self.CTruck)))
+    return 
+
+
+
