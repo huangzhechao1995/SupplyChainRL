06169b75351fcf4a137978af7f5914f927f7730f
Zhechao <huangzhechao1995@hotmail.com>
2019-05-17 21:45:42 -0400

    modified baseline model
    guarantee that the factory storage does not go negative.

--- baseline.py
+++ baseline.py
@@ -20,8 +20,10 @@ def sigma_Q_policy(stock, sigma, Q):
     assignment: array of int, amount to replenish for each factory  
     '''    
     assignment = np.zeros([len(stock)])
-    assignment[1:] = np.where(stock[1:] < sigma[1:], Q[1:],0)
-    assignment[0] = (stock[0] - np.sum(assignment[1:]) < sigma[0]) * Q[0]
+    assignment[1:] = np.where(stock[1:] <= sigma[1:], Q[1:],0)
+    assignment[0] = ((stock[0] - np.sum(assignment[1:])) <= sigma[0]) * Q[0]
+    if stock[0]-np.sum(assignment[1:])<0:
+        assignment[1:]=0
     return assignment 
 
 
@@ -52,6 +54,7 @@ def run_episode(num_iter, sigma, Q):
         (next_state,reward,terminal) = game.step_game(action_arr)
         reward_vec.append(reward)
         epi_reward += reward*GAMMA**t
+
         t += 1
 
     return epi_reward, reward_vec
@@ -63,10 +66,12 @@ def grid_search_reward(num_iter, CWarehouse, num_obj):
     best_index_avg = (1,1)
     best_index_epi = (1,1)
     
-    for i in range(max_cap):
-        for j in range(max_cap):
+    for i in range(1,max_cap):
+        for j in range(1,max_cap):
             sigma = i * np.ones(num_obj)
+            #Q = j * np.ones(num_obj) 
             Q = j * np.ones(num_obj) 
+            Q[0]=np.sum(Q[1:])
             epi_reward, reward_vec = run_episode(num_iter, sigma, Q)
             mean_reward = np.mean(reward_vec)
             
@@ -81,33 +86,43 @@ def grid_search_reward(num_iter, CWarehouse, num_obj):
     return max_epi, max_avg_profit, best_index_avg, best_index_epi
 
 def run_best_param(num_iter, best_index, num_obj):
-    sigma = best_index[0] * np.ones(num_obj)
-    Q = best_index[1] * np.ones(num_obj) 
-    epi_reward, reward_vec = run_episode(num_iter, sigma, Q)
-    
+    epi_list=[]
+    reward_list=[]
+    for i in range(1,10): 
+        sigma = best_index[0] * np.ones(num_obj)
+        Q = best_index[1] * np.ones(num_obj) 
+        Q[0]=np.sum(Q[1:])
+        epi_reward, reward_vec = run_episode(num_iter, sigma, Q)
+        epi_list.append(epi_reward)
+        reward_list.append(np.mean(reward_vec))
+
     print("Best Policy Results")
-    print('epi reward:', round(epi_reward,3))
-    print('avg reward:', round(np.mean(reward_vec),3))
-    plt.scatter(range(num_iter), reward_vec)
+    print('epi reward:', round(sum(epi_list)/len(epi_list),3))
+    print('avg reward:', round(sum(reward_list)/len(reward_list),3))
+        
 
 if __name__ == '__main__':
     
+
+
     K = 2
-    Kpr = 5
+    Kpr = 1
     Kst = np.array([0, 0, 0]).reshape(K+1)
-    Kpe = 5
+    Kpe = 1
     Ktr = np.array([np.nan, 0, 0]).reshape(K+1)
-    CWarehouse = np.array([20, 20, 20]).reshape(K+1)
-    CTruck = np.array([np.nan, 10, 10]).reshape(K+1)
-    Price = 10
+    CWarehouse = np.array([10, 6, 6]).reshape(K+1)
+    CTruck = np.array([np.nan, 6, 6]).reshape(K+1)
+    Price = 3
     dmax = 2
+
+
     GAMMA = 0.5
     
     num_obj = len(CWarehouse)
-    num_iter = 36
+    num_iter = 12
     max_epi, max_avg_profit, best_index_avg, best_index_epi = grid_search_reward(num_iter, CWarehouse, num_obj)
     
     print('max epi reward:', round(max_epi,3))
     print('max avg reward:', round(max_avg_profit,3))
     print('----------')
-    run_best_param(num_iter, best_index_avg, num_obj)
+    run_best_param(num_iter, (0,2), num_obj)
